f2d25970330cbb40b8ef6595f85bf9dc95173ff0
Pabitra Nahak <pabitrakumarnahak@gmail.com>
2024-05-20 22:22:49 +0530

    db string updated

--- db.js
+++ db.js
@@ -1,18 +1,20 @@
-const mongoose = require('mongoose');
-// const url = 'mongodb://localhost:27017/QuickNotes'; // Local Database
-const url = 'mongodb+srv://pabitrakumarnahak22:gQ6LIKgTsZn3mIab@vivid-tech-hub-db.8czxlhn.mongodb.net/'; // Cloud Database
+const mongoose = require("mongoose");
+const url = "mongodb://localhost:27017/VividTechHubDB"; // Local Database
+// const url = 'mongodb+srv://pabitrakumarnahak22:gQ6LIKgTsZn3mIab@vivid-tech-hub-db.8czxlhn.mongodb.net/'; // Cloud Database
 
 const connectToMongo = () => {
-    try {
-        mongoose.set('strictQuery', true);
-        mongoose.connect(url);
-        // console.log('Mongo DB connected!');
-        console.log("%c Mongo DB 🚙 Connected", "color:blue; font-size:50px");
-    } catch (err) {
-        console.log(error)
-        process.exit()
-    }
+  try {
+    mongoose.set("strictQuery", true);
+    mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    // console.log('Mongo DB connected!');
+    console.log("%c Mongo DB 🚙 Connected", "color:blue; font-size:50px");
+  } catch (err) {
+    console.log(error);
+    process.exit();
+  }
 };
-
 
 module.exports = connectToMongo;
